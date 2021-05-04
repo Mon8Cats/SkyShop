b0d0e5b38e098f52832cf68a6f8dce5e2e2a4197
StoneRoot <stoneroot@StoneRoots-MBP.lan>
2021-05-04 17:15:17 -0500

    API - Validation

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -6,13 +6,31 @@ namespace API.Extensions
 {
     public static class SwaggerServiceExtensions
     {
-        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection  services)
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement{{securitySchema, new[] {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
             });
-            
+
             return services;
         }
 
@@ -20,7 +38,7 @@ namespace API.Extensions
         {
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            
+
             return app;
         }
     }
